065c4dbf13e113c7caeee5b7c5f81d23031a2b17
Hoang Nguyen <hdnguyen0305@gmail.com>
2022-07-01 15:29:49 +0700

    update payment page

--- src/components/confirm/index.tsx
+++ src/components/confirm/index.tsx
@@ -1,7 +1,13 @@
+import payment_success from 'utils/assets/images/payment_success.jpg'
 
 const Confirm = () => {
   return (
-    <div>Juice, Squeeze</div>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+
+        <p className="text-4xl font-['Muli-ExtraBold'] text-blue-500 -mt-40 mb-10">Thanh toán thành công</p>
+        <img src={payment_success} className="w-96" alt="" />
+        <p className="text-lg text-gray-500 mt-12">Mã thanh toán: PAY-R2EYLd6KTTGf8s8vVp47_g</p>
+    </div>
   )
 }
 
